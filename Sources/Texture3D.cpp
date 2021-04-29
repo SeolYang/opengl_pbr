80076e16d38b4b15ed5defdaed117861bf63a1ec
SeolYang <kyowonhorang@gmail.com>
2021-04-29 14:03:23 +0900

    Texture3D를 추가하였습니다

---
+++ Sources/Texture3D.cpp
@@ -0,0 +1,36 @@
+﻿#include "Texture3D.h"
+
+Texture3D::Texture3D(const std::vector<GLfloat>& rawData, unsigned int width, unsigned int height, unsigned int depth, Sampler3D sampler, unsigned int maxMipLevel, bool bGenerateMip)
+{
+   glGenTextures(1, &m_id);
+   glBindTexture(GL_TEXTURE_3D, m_id);
+
+   glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, sampler.MinFilter);
+   glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, sampler.MagFilter);
+
+   glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, sampler.WrapS);
+   glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, sampler.WrapR);
+   glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, sampler.WrapT);
+
+   glTexStorage3D(GL_TEXTURE_3D, maxMipLevel, GL_RGBA8, width, height, depth);
+   glTexImage3D(GL_TEXTURE_3D, 0, GL_RGBA8, width, height, depth, 0, GL_RGBA, GL_FLOAT, rawData.data());
+   if (bGenerateMip)
+   {
+      glGenerateMipmap(GL_TEXTURE_3D);
+   }
+
+   glBindTexture(GL_TEXTURE_3D, 0);
+}
+
+void Texture3D::Bind(unsigned int slot)
+{
+   glActiveTexture(GL_TEXTURE0 + slot);
+   glBindTexture(GL_TEXTURE_3D, m_id);
+}
+
+void Texture3D::Unbind(unsigned slot)
+{
+   glActiveTexture(GL_TEXTURE0 + slot);
+   glBindTexture(GL_TEXTURE_3D, 0);
+}
+
---
+++ Sources/Texture3D.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Rendering.h"
+
+struct Sampler3D
+{
+   GLint MinFilter = GL_LINEAR_MIPMAP_LINEAR;
+   GLint MagFilter = GL_NEAREST;
+   GLint WrapS = GL_CLAMP_TO_BORDER;
+   GLint WrapT = GL_CLAMP_TO_BORDER;
+   GLint WrapR = GL_CLAMP_TO_BORDER;
+};
+
+class Texture3D
+{
+public:
+   Texture3D(const std::vector<GLfloat>& rawData, unsigned int width, unsigned int height, unsigned int depth, Sampler3D sampler = Sampler3D(), unsigned int maxMipLevel = 7, bool bGenerateMip = true);
+   ~Texture3D() = default;
+
+   void Bind(unsigned int slot);
+   void Unbind(unsigned int slot);
+
+   unsigned int GetID() const { return m_id; }
+
+private:
+   unsigned int m_id = 0;
+   
+};
