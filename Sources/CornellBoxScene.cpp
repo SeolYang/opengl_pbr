dc8d557dea1bd0ef62c0b4d33ff16d54b754cc6c
SeolYang <kyowonhorang@gmail.com>
2021-05-07 15:12:06 +0900

    Improve quality of cornell box scene

--- Sources/CornellBoxScene.cpp
+++ Sources/CornellBoxScene.cpp
@@ -15,101 +15,139 @@
 
 void CornellBoxScene::Construct()
 {
-	const ModelLoadParams quadLoadParams{
+	constexpr float scale = 10.0f;
+	Model* backPlane = CreatePlane("Back");
+	{
+		backPlane->SetPosition(glm::vec3(0.0f, 0.0f, -scale));
+		backPlane->SetScale(glm::vec3(scale));
+
+		Material* backPlaneMat = backPlane->GetMaterial(0);
+		backPlaneMat->SetForceFactor(EMaterialTexture::BaseColor, true);
+		backPlaneMat->SetBaseColorFactor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+		backPlaneMat->SetForceFactor(EMaterialTexture::Emissive, true);
+		backPlaneMat->SetEmissiveFactor(glm::vec3(0.0f, 0.0f, 0.0f));
+		backPlaneMat->SetForceFactor(EMaterialTexture::MetallicRoughness, true);
+		backPlaneMat->SetMetallicFactor(0.0f);
+		backPlaneMat->SetRoughnessFactor(1.0f);
+	}
+
+	Model* leftPlane = CreatePlane("Left");
+	{
+		leftPlane->SetPosition(glm::vec3(-scale, 0.0f, 0.0f));
+		leftPlane->SetScale(glm::vec3(scale));
+		leftPlane->SetRotation(glm::rotate(
+			glm::quat(),
+			glm::radians(90.0f),
+			glm::vec3(0.0f, 1.0f, 0.0f)));
+
+		Material* leftPlaneMat = leftPlane->GetMaterial(0);
+		leftPlaneMat->SetForceFactor(EMaterialTexture::BaseColor, true);
+		leftPlaneMat->SetBaseColorFactor(glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
+		leftPlaneMat->SetForceFactor(EMaterialTexture::Emissive, true);
+		leftPlaneMat->SetEmissiveFactor(glm::vec3(0.0f, 0.0f, 0.0f));
+		leftPlaneMat->SetForceFactor(EMaterialTexture::MetallicRoughness, true);
+		leftPlaneMat->SetMetallicFactor(0.0f);
+		leftPlaneMat->SetRoughnessFactor(1.0f);
+	}
+
+	Model* rightPlane = CreatePlane("Right");
+	{
+		rightPlane->SetPosition(glm::vec3(scale, 0.0f, 0.0f));
+		rightPlane->SetScale(glm::vec3(scale));
+		rightPlane->SetRotation(glm::rotate(
+			glm::quat(),
+			glm::radians(-90.0f),
+			glm::vec3(0.0f, 1.0f, 0.0f)));
+
+		Material* rightPlaneMat = rightPlane->GetMaterial(0);
+		rightPlaneMat->SetForceFactor(EMaterialTexture::BaseColor, true);
+		rightPlaneMat->SetBaseColorFactor(glm::vec4(0.0f, 1.0f, 0.0f, 1.0f));
+		rightPlaneMat->SetForceFactor(EMaterialTexture::Emissive, true);
+		rightPlaneMat->SetEmissiveFactor(glm::vec3(0.0f, 0.0f, 0.0f));
+		rightPlaneMat->SetForceFactor(EMaterialTexture::MetallicRoughness, true);
+		rightPlaneMat->SetMetallicFactor(0.0f);
+		rightPlaneMat->SetRoughnessFactor(1.0f);
+	}
+
+	Model* topPlane = CreatePlane("Top");
+	{
+		topPlane->SetPosition(glm::vec3(0.0f, scale, 0.0f));
+		topPlane->SetScale(glm::vec3(scale));
+		topPlane->SetRotation(glm::rotate(
+			glm::quat(),
+			glm::radians(90.0f),
+			glm::vec3(1.0f, 0.0f, 0.0f)));
+
+		Material* topPlaneMat = topPlane->GetMaterial(0);
+		topPlaneMat->SetForceFactor(EMaterialTexture::BaseColor, true);
+		topPlaneMat->SetBaseColorFactor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+		topPlaneMat->SetForceFactor(EMaterialTexture::Emissive, true);
+		topPlaneMat->SetEmissiveFactor(glm::vec3(0.0f, 0.0f, 0.0f));
+		topPlaneMat->SetForceFactor(EMaterialTexture::MetallicRoughness, true);
+		topPlaneMat->SetMetallicFactor(0.0f);
+		topPlaneMat->SetRoughnessFactor(1.0f);
+	}
+
+
+	Model* bottomPlane = CreatePlane("Bottom");
+	{
+		bottomPlane->SetPosition(glm::vec3(0.0f, -scale, 0.0f));
+		bottomPlane->SetScale(glm::vec3(scale));
+		bottomPlane->SetRotation(glm::rotate(
+			glm::quat(),
+			glm::radians(-90.0f),
+			glm::vec3(1.0f, 0.0f, 0.0f)));
+
+		Material* bottomPlaneMat = bottomPlane->GetMaterial(0);
+		bottomPlaneMat->SetForceFactor(EMaterialTexture::BaseColor, true);
+		bottomPlaneMat->SetBaseColorFactor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+		bottomPlaneMat->SetForceFactor(EMaterialTexture::Emissive, true);
+		bottomPlaneMat->SetEmissiveFactor(glm::vec3(0.0f, 0.0f, 0.0f));
+		bottomPlaneMat->SetForceFactor(EMaterialTexture::MetallicRoughness, true);
+		bottomPlaneMat->SetMetallicFactor(0.0f);
+		bottomPlaneMat->SetRoughnessFactor(1.0f);
+	}
+
+	const ModelLoadParams sphereLoadParams{
 	.CalcTangentSpace = true,
-	.ConvertToLeftHanded = true,
+	.ConvertToLeftHanded = false,
 	.GenSmoothNormals = true,
 	.GenUVs = true,
-	.PreTransformVertices = false,
+	.PreTransformVertices = true,
 	.Triangulate = true
 	};
 
-	constexpr float scale = 10.0f;
-	Model* backPlane = CreatePlane("Back");
-	backPlane->SetPosition(glm::vec3(0.0f, 0.0f, -scale));
-	backPlane->SetScale(glm::vec3(scale));
-
-	Material* backPlaneMat = backPlane->GetMaterial(0);
-	backPlaneMat->SetForceFactor(EMaterialTexture::BaseColor, true);
-	backPlaneMat->SetBaseColorFactor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
-	backPlaneMat->SetForceFactor(EMaterialTexture::Emissive, true);
-	backPlaneMat->SetEmissiveFactor(glm::vec3(0.0f, 0.0f, 0.0f));
-
-	Model* leftPlane = CreatePlane("Left");
-	leftPlane->SetPosition(glm::vec3(-scale, 0.0f, 0.0f));
-	leftPlane->SetScale(glm::vec3(scale));
-	leftPlane->SetRotation(glm::rotate(
-		glm::quat(),
-		glm::radians(90.0f),
-		glm::vec3(0.0f, 1.0f, 0.0f)));
-
-	Material* leftPlaneMat = leftPlane->GetMaterial(0);
-	leftPlaneMat->SetForceFactor(EMaterialTexture::BaseColor, true);
-	leftPlaneMat->SetBaseColorFactor(glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
-	leftPlaneMat->SetForceFactor(EMaterialTexture::Emissive, true);
-	leftPlaneMat->SetEmissiveFactor(glm::vec3(0.0f, 0.0f, 0.0f));
-	leftPlaneMat->SetForceFactor(EMaterialTexture::MetallicRoughness, true);
-	leftPlaneMat->SetMetallicFactor(0.0f);
-	leftPlaneMat->SetRoughnessFactor(1.0f);
-
-	Model* rightPlane = CreatePlane("Right");
-	rightPlane->SetPosition(glm::vec3(scale, 0.0f, 0.0f));
-	rightPlane->SetScale(glm::vec3(scale));
-	rightPlane->SetRotation(glm::rotate(
-		glm::quat(),
-		glm::radians(-90.0f),
-		glm::vec3(0.0f, 1.0f, 0.0f)));
-
-	Material* rightPlaneMat = rightPlane->GetMaterial(0);
-	rightPlaneMat->SetForceFactor(EMaterialTexture::BaseColor, true);
-	rightPlaneMat->SetBaseColorFactor(glm::vec4(0.0f, 1.0f, 0.0f, 1.0f));
-	rightPlaneMat->SetForceFactor(EMaterialTexture::Emissive, true);
-	rightPlaneMat->SetEmissiveFactor(glm::vec3(0.0f, 0.0f, 0.0f));
-	rightPlaneMat->SetForceFactor(EMaterialTexture::MetallicRoughness, true);
-	rightPlaneMat->SetMetallicFactor(0.0f);
-	rightPlaneMat->SetRoughnessFactor(1.0f);
-
-	Model* topPlane = CreatePlane("Top");
-	topPlane->SetPosition(glm::vec3(0.0f, scale, 0.0f));
-	topPlane->SetScale(glm::vec3(scale));
-	topPlane->SetRotation(glm::rotate(
-		glm::quat(),
-		glm::radians(90.0f),
-		glm::vec3(1.0f, 0.0f, 0.0f)));
-
-	Material* topPlaneMat = topPlane->GetMaterial(0);
-	topPlaneMat->SetForceFactor(EMaterialTexture::BaseColor, true);
-	topPlaneMat->SetBaseColorFactor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
-	topPlaneMat->SetForceFactor(EMaterialTexture::Emissive, true);
-	topPlaneMat->SetEmissiveFactor(glm::vec3(0.0f, 0.0f, 0.0f));
-	topPlaneMat->SetForceFactor(EMaterialTexture::MetallicRoughness, true);
-	topPlaneMat->SetMetallicFactor(0.0f);
-	topPlaneMat->SetRoughnessFactor(1.0f);
-
-	Model* bottomPlane = CreatePlane("Bottom");
-	bottomPlane->SetPosition(glm::vec3(0.0f, -scale, 0.0f));
-	bottomPlane->SetScale(glm::vec3(scale));
-	bottomPlane->SetRotation(glm::rotate(
-		glm::quat(),
-		glm::radians(-90.0f),
-		glm::vec3(1.0f, 0.0f, 0.0f)));
-
-	Material* bottomPlaneMat = bottomPlane->GetMaterial(0);
-	bottomPlaneMat->SetForceFactor(EMaterialTexture::BaseColor, true);
-	bottomPlaneMat->SetBaseColorFactor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
-	bottomPlaneMat->SetForceFactor(EMaterialTexture::Emissive, true);
-	bottomPlaneMat->SetEmissiveFactor(glm::vec3(0.0f, 0.0f, 0.0f));
-	bottomPlaneMat->SetForceFactor(EMaterialTexture::MetallicRoughness, true);
-	bottomPlaneMat->SetMetallicFactor(0.0f);
-	bottomPlaneMat->SetRoughnessFactor(1.0f);
+	auto areaLight = CreatePlane("AreaLight");
+	{
+		areaLight->SetPosition(glm::vec3(0.0f, scale - 0.01f, 0.0f));
+		areaLight->SetScale(glm::vec3(scale/4.0f));
+		areaLight->SetRotation(glm::rotate(
+			glm::quat(),
+			glm::radians(90.0f),
+			glm::vec3(1.0f, 0.0f, 0.0f)));
+		areaLight->bCastShadow = false;
+		auto material = areaLight->GetMaterial(0);
+		material->SetForceFactor(EMaterialTexture::Emissive, true);
+		material->SetForceFactor(EMaterialTexture::BaseColor, true);
+		material->SetBaseColorFactor(glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
+		material->SetEmissiveFactor(glm::vec3(3.0f));
+		material->SetForceFactor(EMaterialTexture::MetallicRoughness, true);
+		material->SetMetallicFactor(0.0f);
+		material->SetRoughnessFactor(0.0f);
+	}
 
 	m_mainLight = this->CreateLight("Sun");
-	m_mainLight->SetIntensity(glm::vec3{ 5.0f });
-	m_mainLight->bUseRotation = false;
-	m_mainLight->SetPosition(glm::vec3(0.0f, 0.0f, 1.0f));
-	this->UpdateLightRotation();
+	{
+		m_mainLight->SetIntensity(glm::vec3{ 0.0f });
+		m_mainLight->bUseRotation = false;
+		m_mainLight->SetPosition(glm::vec3(0.0f, 0.0f, 1.0f));
+		this->UpdateLightRotation();
+	}
 
 	auto mainCamera = this->GetMainCamera();
+	{
+		mainCamera->SetPosition(glm::vec3(0.0f, 0.0f, 30.0f));
+	}
 }
 
 void CornellBoxScene::Update(float dt)
